3b1284ec95862e686cfd1f02fb0a931fd44f05a6
Timothy J. Warren <tim@timshomepage.net>
2014-10-24 10:30:54 -0400

    A lot more tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,14 @@
 module.exports = function(grunt) {
 	'use strict';
 
+	var tests = 'tests/**/*_test.js';
+	var src = 'lib/**/*.js';
+	var reportDir = 'coverage';
+
 	// Project configuration
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		clean: [ 'build' ],
 		jsdoc: {
 			dist: {
 				src: ['lib/*.js', 'README.md'],
@@ -15,18 +20,45 @@ module.exports = function(grunt) {
 			}
 		},
 		nodeunit: {
-			all: ['tests/**/*_test.js'],
+			all: [tests],
 			options: {
 				reporter: 'verbose'
+			}
+		},
+		watch: {
+			files: [src, tests],
+			tasks: 'default'
+		},
+		instrument: {
+			files: src,
+			options: {
+				lazy: true,
+				basePath : 'build/instrument/'
+			}
+		},
+		storeCoverage : {
+			options : {
+				dir: reportDir
+			}
+		},
+		makeReport: {
+			src: 'build/reports/**/*.json',
+			options: {
+				type: ['lcov', 'html'],
+				dir: reportDir,
+				print: 'detail'
 			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-jsdoc');
+	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	grunt.loadNpmTasks('grunt-istanbul');
 
 
 	grunt.registerTask('default', ['nodeunit','jsdoc']);
 	grunt.registerTask('tests', 'nodeunit');
 	grunt.registerTask('docs', 'jsdoc');
+	grunt.registerTask('cover', ['clean', 'instrument', 'tests', 'storeCoverage', 'makeReport']);
 };
--- tests/query-builder-base.js
+++ tests/query-builder-base.js
@@ -85,16 +85,160 @@ module.exports = (function()  {
 					.get(base.testCallback.bind(test, test));
 
 				test.done();
+			},
+			'Select get': function(test) {
+				base.qb.select('id, key as k, val')
+					.get('create_test', 2, 1, base.testCallback.bind(test, test));
+
+				test.done();
+			},
+			'Select from get': function(test) {
+				base.qb.select('id, key as k, val')
+					.from('create_test ct')
+					.where('id >', 1)
+					.get(base.testCallback.bind(test, test));
+
+				test.done();
+			},
+			'Select from limit get': function(test) {
+				base.qb.select('id, key as k, val')
+					.from('create_test ct')
+					.where('id >', 1)
+					.limit(3)
+					.get(base.testCallback.bind(test, test));
+
+				test.done();
 			}
 		},
 		'Grouping tests' : {
+			'Using grouping method': function(test) {
+				base.qb.select('id, key as k, val')
+					.from('create_test')
+					.groupStart()
+					.where('id >', 1)
+					.where('id <', 900)
+					.groupEnd()
+					.limit(2, 1)
+					.get(base.testCallback.bind(test, test));
 
+				test.done();
+			},
+			'Using or grouping method': function(test) {
+				base.qb.select('id, key as k, val')
+					.from('create_test')
+					.groupStart()
+					.where('id >', 1)
+					.where('id <', 900)
+					.groupEnd()
+					.orGroupStart()
+					.where('id', 0)
+					.groupEnd()
+					.limit(2, 1)
+					.get(base.testCallback.bind(test, test));
+
+				test.done();
+			},
+			'Using or not grouping method': function(test) {
+				base.qb.select('id, key as k, val')
+					.from('create_test')
+					.groupStart()
+					.where('id >', 1)
+					.where('id <', 900)
+					.groupEnd()
+					.orNotGroupStart()
+					.where('id', 0)
+					.groupEnd()
+					.limit(2, 1)
+					.get(base.testCallback.bind(test, test));
+
+				test.done();
+			}
 		},
 		'Where in tests' : {
+			'Where in': function(test) {
+				base.qb.from('create_test')
+					.whereIn('id', [0, 6, 56, 563, 341])
+					.get(base.testCallback.bind(test, test));
 
+				test.done();
+			},
+			'Or Where in': function(test) {
+				base.qb.from('create_test')
+					.where('key', 'false')
+					.orWhereIn('id', [0, 6, 56, 563, 341])
+					.get(base.testCallback.bind(test, test));
+
+				test.done();
+			},
+			'Where Not in': function(test) {
+				base.qb.from('create_test')
+					.where('key', 'false')
+					.whereNotIn('id', [0, 6, 56, 563, 341])
+					.get(base.testCallback.bind(test, test));
+
+				test.done();
+			},
+			'Or Where Not in': function(test) {
+				base.qb.from('create_test')
+					.where('key', 'false')
+					.orWhereNotIn('id', [0, 6, 56, 563, 341])
+					.get(base.testCallback.bind(test, test));
+
+				test.done();
+			}
 		},
 		'Query modifier tests': {
+			'Order By': function(test) {
+				base.qb.select('id, key as k, val')
+					.from('create_test')
+					.where('id >', 0)
+					.where('id <', 9000)
+					.orderBy('id', 'DESC')
+					.orderBy('k', "ASC")
+					.limit(5, 2)
+					.get(base.testCallback.bind(test, test));
 
+				test.done();
+			},
+			'Group by': function(test) {
+				base.qb.select('id, key as k, val')
+					.from('create_test')
+					.where('id >', 0)
+					.where('id <', 9000)
+					.groupBy('k')
+					.groupBy(['id', 'val'])
+					.orderBy('id', 'DESC')
+					.orderBy('k', "ASC")
+					.limit(5, 2)
+					.get(base.testCallback.bind(test, test));
+
+				test.done();
+			},
+			'Or Where': function(test) {
+				base.qb.select('id, key as k, val')
+					.from('create_test')
+					.where(' id ', 1)
+					.orWhere('key >', 0)
+					.limit(2, 1)
+					.get(base.testCallback.bind(test, test));
+
+				test.done();
+			},
+			'Like' : function(test) {
+				base.qb.from('create_test')
+					.like('key', 'og')
+					.get(base.testCallback.bind(test, test));
+
+				test.done();
+			},
+			'Or Like': function(test) {
+				base.qb.from('create_test')
+					.like('key', 'og')
+					.orLike('key', 'val')
+					.get(base.testCallback.bind(test, test));
+
+				test.done();
+			}
 		},
 		'DB update tests' : {
 
