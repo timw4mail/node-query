0ee092abd9ee9df756b854cc03a91f52899a12d5
Timothy J Warren <tim@timshomepage.net>
2016-11-14 20:23:27 -0500

    Completely promisify firebird driver

--- test/adapters/node-firebird_test.js
+++ test/adapters/node-firebird_test.js
@@ -1,42 +1,40 @@
 'use strict';
 
-(() => {
-	// Load the test base
-	const path = require('path');
-	const reload = require('require-reload')(require);
-	const testBase = reload('../base');
-	const expect = reload('chai').expect;
-	const testRunner = testBase.promiseTestRunner;
+// Load the test base
+const path = require('path');
+const reload = require('require-reload')(require);
+const testBase = reload('../base');
+const expect = testBase.expect;
+const testRunner = testBase.promiseTestRunner;
 
-	// Skip on CI
-	if (process.env.CI || process.env.TRAVIS) {
-		return;
-	}
+// Skip on CI
+if (process.env.CI || process.env.TRAVIS) {
+	return;
+}
 
-	// Load the test config file
-	let adapterName = 'node-firebird';
-	let Firebird = reload(adapterName);
-	const config = reload('../config.json')[adapterName];
-	config.connection.database = path.join(__dirname, config.connection.database);
-	let nodeQuery = reload('../../lib/NodeQuery')(config);
+// Load the test config file
+let adapterName = 'node-firebird';
+let Firebird = reload(adapterName);
+const config = reload('../config.json')[adapterName];
+config.connection.database = path.join(__dirname, config.connection.database);
+let nodeQuery = reload('../../lib/NodeQuery')(config);
 
-	let qb = nodeQuery.getQuery();
+let qb = nodeQuery.getQuery();
 
-	suite('Firebird adapter tests -', () => {
-		test('nodeQuery.getQuery = nodeQuery.init', () => {
-			expect(nodeQuery.getQuery())
-				.to.be.deep.equal(qb);
-		});
-		test('insertBatch throws error', () => {
-			expect(() => {
-				qb.driver.insertBatch('create_test', []);
-			}).to.throw(Error, 'Not Implemented');
-		});
-
-		testRunner(qb);
-
-		suiteTeardown(() => {
-			qb.end();
-		});
+suite('Firebird adapter tests -', () => {
+	test('nodeQuery.getQuery = nodeQuery.init', () => {
+		expect(nodeQuery.getQuery())
+			.to.be.deep.equal(qb);
 	});
-})();
+	test('insertBatch throws error', () => {
+		expect(() => {
+			qb.driver.insertBatch('create_test', []);
+		}).to.throw(Error, 'Not Implemented');
+	});
+
+	testRunner(qb);
+
+	suiteTeardown(() => {
+		qb.end();
+	});
+});
