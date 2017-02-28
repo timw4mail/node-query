806a1e1702cc66a4688ba9a4251e839421128f41
Timothy J Warren <tim@timshomepage.net>
2017-02-28 16:43:09 -0500

    Try gitlab ci setup again

--- test/docker_install.sh
+++ test/docker_install.sh
@@ -8,7 +8,6 @@ set -xe
 # Install sqlite3
 echo -e 'http://dl-cdn.alpinelinux.org/alpine/edge/main\nhttp://dl-cdn.alpinelinux.org/alpine/edge/community\nhttp://dl-cdn.alpinelinux.org/alpine/edge/testing' > /etc/apk/repositories
 apk add --no-cache git yarn sqlite
-npm install pg sqlite3 dblite mysql2
 
 # Replace test config with docker config file
 mv "./test/config-ci.json" "./test/config.json"
